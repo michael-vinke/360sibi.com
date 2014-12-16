3a95f9276e47863261fa1c4e0aa83d7e533a3438
MikeCoder <mikecoder13@gmail.com>
2014-12-16 09:51:00 +0800

    new model

---
+++ model/user.php
@@ -0,0 +1,17 @@
+<?php
+   /**
+    * Class user
+    * @author Mike mikecoder.cn@gmail.com
+    * @date 2014-12-16
+    */
+   class user {
+      public function __construct($options) {
+         // code
+      }
+
+      public function storeUser($userId, $userName){
+
+      }
+   }
+
+?>
