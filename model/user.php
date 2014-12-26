8636c061e058a8df98e3ac9193cf03a8158f29c1
Fenice <kevin_samuel@foxmail.com>
2014-12-27 01:17:48 +0800

    change user.php

--- model/user.php
+++ model/user.php
@@ -4,6 +4,8 @@
     * @author Mike mikecoder.cn@gmail.com
     * @date 2014-12-16
     */
+   require_once ('DBoper.class.php');
+
    class user {
       public function __construct($options) {
          // code
