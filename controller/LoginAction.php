eaf6ec0bc60b206212b866372ede563811584ada
Fenice <kevin_samuel@foxmail.com>
2014-12-28 14:34:57 +0800

    add Action.class.php

--- controller/LoginAction.php
+++ controller/LoginAction.php
@@ -1,3 +1,17 @@
 <?php
+	
+	/**
+	* Action Login
+	* @author fenicesun <kevin.samuel.sun@gmail.com>
+	* @date 2014/12/28
+	* deal with the request and reponse of login
+	*
+	*/
+	require_once ('../model/user.php');
+	require_once ('../includes/Action.class.php');
+
+	class Login extends Action {
+
+	}	
 
 ?>
---
+++ includes/Action.class.php
@@ -0,0 +1,52 @@
+<?php
+	
+	/**
+	* Action 
+	* @author fenicesun <kevin.samuel.sun@gmail.com>
+	* @date 2014/12/28
+	* Action base class
+	*
+	*/
+	require_once ('ReturnInfo.class.php');
+
+	class Action {
+
+		protected $request_data;
+
+		public function __construct() {
+			$this->request_data = $_REQUEST;		
+		}
+
+		/**
+		* get request data by key, return $_REQUEST['key']
+		* @param $key
+		*
+		*/
+		public function get_request_by_key($key) {
+			return $this->request_data['$key'];	
+		}
+
+		/**
+		* put response data in json
+		* @param $data
+		*
+		*/
+		public function put_response_in_json($data) {
+			return json_encode($data);
+		}
+
+		/**
+		* override this function in sub class
+		* dispatch request handle by $action
+		* @param $action
+		* @param $param
+		*
+		*/		
+		public function dispatch($action, $param) {
+
+		}								
+
+
+	}	
+
+?>
